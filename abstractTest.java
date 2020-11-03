c932511e2d7ce45d02d74931bd11441814935958
yamiphiri <yamikanidj@gmail.com>
2020-11-03 08:45:33 +0200

    init commit

---
+++ abstractTest.java
@@ -0,0 +1,25 @@
+// Abstract class
+abstract class Animal {
+  
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+  public void animalSound() {
+    
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+
+
+class abstractTest {
+  public static void main(String[] args) {
+    // Animal X = new Animal();
+    Pig myPig = new Pig(); // Create a Pig object
+    // Dog dog = new Dog();
+    myPig.animalSound();
+   
+    
+  }
+}
